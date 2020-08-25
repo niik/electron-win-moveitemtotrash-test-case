09050abc3c3c34e725d030908f84697417af4ab7
Markus Olsson <j.markus.olsson@gmail.com>
2020-08-25 09:33:29 -0700

    Minimal test case

--- renderer.js
+++ renderer.js
@@ -4,3 +4,9 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const { shell, remote } = require('electron')
+
+const appExePath = remote.app.getPath('exe');
+console.log(`Attempting to move the current process exe (${appExePath}) to trash, should fail`)
+console.assert(!shell.moveItemToTrash(appExePath), `moveItemToTrash returned true`)
